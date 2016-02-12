db801605cebea85ef1f3a5822b4cb34dec0bca08
Thomas Sorensen <thomaspsorensen@gmail.com>
2016-02-12 10:18:26 -0800

    [Feature] Modify: added API key

--- server/entertain/entertainController.js
+++ server/entertain/entertainController.js
@@ -7,11 +7,12 @@ var request = require('request')
 
 module.exports = {
   getAll: function(req, res, next) {
-    console.log("get all is working!")
-    request('http://www.theonion.com', function(error, response, body) {
+    console.log("getAll is working!")
+    request('http://data.tmsapi.com/v1.1/movies/showings?startDate=2016-02-12&zip=78701&api_key=4d9qk2nptvxkfg2bydfhrdrn>', function(error, response, body) {
       if(!error && response.statusCode === 200) {
         console.log("this is the response from the server", body)
       }
     })
   }
 }
+
