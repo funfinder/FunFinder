a59ed6dfb2be2a56f348b5ffb46ed9b03cb33872
Thomas Sorensen <thomaspsorensen@gmail.com>
2016-02-11 20:01:44 -0800

    [Feature] Add: added routes and controllers

--- server/entertain/entertainController.js
+++ server/entertain/entertainController.js
@@ -1,1 +1,17 @@
+var request = require('request')
+
 //add methods I might need
+
+//call the api here
+//give the response from the api to the model on the public side
+
+module.exports = {
+  getAll: function(req, res, next) {
+    console.log("get all is working!")
+    request('http://www.theonion.com', function(error, response, body) {
+      if(!error && response.statusCode === 200) {
+        console.log("this is the response from the server", body)
+      }
+    })
+  }
+}
