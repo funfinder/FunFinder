22bcdd42b48a422e780781e86beca26ae412e8ad
Jason Cheung <jasoncheungcf@gmail.com>
2016-02-12 17:52:03 -0800

    [Key] Update key

--- server/entertain/entertainController.js
+++ server/entertain/entertainController.js
@@ -7,8 +7,8 @@ var request = require('request')
 
 module.exports = {
   getAll: function(req, res, next) {
-    
-    request('http://data.tmsapi.com/v1.1/movies/showings?startDate=2016-02-12&zip=78701&api_key=4d9qk2nptvxkfg2bydfhrdrn', function(error, response, body) {
+
+    request('http://data.tmsapi.com/v1.1/movies/showings?startDate=2016-02-12&zip=78701&api_key=', function(error, response, body) {
       if(!error && response.statusCode === 200) {
         var movieArray = [];
         var convertTime = function(time) {
@@ -27,7 +27,7 @@ module.exports = {
           var description = body[i].shortDescription || "no description available";
           var convertedTime = convertTime(body[i].showtimes[0].dateTime.slice(11))
           console.log("body[i] ", body[i]);
-          movie.title = body[i].title; 
+          movie.title = body[i].title;
           movie.description = description;
           movie.name = body[i].showtimes[0].theatre.name;
           movie.time = convertedTime;
