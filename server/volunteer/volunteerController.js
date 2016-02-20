68a3401f934bf90c8f16348a3bae0a224fbcfe8a
Nadine Ott <nadineott7@yahoo.com>
2016-02-19 16:22:48 -0800

    [volunteer.html] modified

--- server/volunteer/volunteerController.js
+++ server/volunteer/volunteerController.js
@@ -8,10 +8,11 @@ module.exports = {
     console.log('volunteer getAll called')
     request('https://www.eventbriteapi.com/v3/events/search/?token=YGAXTF3CVBJD74VGIJVL&q=%22volunteer%22&location.address=%22San%20Francisco%22', function(error, response, body) {
       if (!error && response.statusCode === 200) {
-        var output = [];
+        var volOpsArray = [];
         body = JSON.parse(body);
 
         for (var i = 0; i < 6; i++) {
+          var output = [];
           output.push("EVENT " + i);
           //push name of event to output array
            output.push(body.events[i]["name"]["text"]);
@@ -49,11 +50,11 @@ module.exports = {
             var time = dateAndTime.slice(11, 16);
 
            output.push("Date and Time: " + date + " " + convertTime(time));
-
+           volOpsArray.push(output);
         }
 
       }
-      res.send(output);
+      res.send(volOpsArray);
 
     })
 
