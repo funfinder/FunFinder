62a8affe78ce81f27bc3cef3b0902bb0c5939f0b
Nadine Ott <nadineott7@yahoo.com>
2016-02-22 14:59:34 -0800

    [socialRoutes.js] don't add this change, was just removing console.log

--- server/social/socialRoutes.js
+++ server/social/socialRoutes.js
@@ -5,7 +5,7 @@ var controller=require('./socialController');
 
 //#Modify to use Dependency Injection from route.js
 module.exports = function(router) {
-	console.log("in social routes");
+  console.log("in social routes");
  // router.get('/:city/:state/:beginTime/:endTime',controller.getAll);
   router.get('/:zip/:beginTime/:endTime',controller.getAllZip);
 }
